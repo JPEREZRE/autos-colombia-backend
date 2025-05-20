5fddcea5ec779fdb7302bc16c6253e15d24db5ea
JPEREZRE <juan.perezr@est.iudigital.edu.co>
2025-05-20 16:40:19 -0500

    Actualización del backend: conexión con MongoDB y CRUD funcionando

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
@@ -10,13 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.use('/api/vehiculos', require('./routes/vehiculos'));
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/celdas', require('./routes/celdas'));
+app.use('/api/pagos', require('./routes/pagos'));
+app.use('/api/novedades', require('./routes/novedades'));
+
+
 // Servir los archivos estáticos de React
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+//app.use(express.static(path.join(__dirname, '../autos-colombia-frontend/build')));
 
 // Manejar cualquier ruta y devolver el index.html de React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
+//app.get('*', (req, res) => {
+  //res.sendFile(path.join(__dirname, '../autos-colombia-frontend/build', 'index.html'));
+//});
 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -26,7 +35,7 @@ mongoose.connect(process.env.MONGO_URI)
   console.log('✅ Conectado a MongoDB');
 
   
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
   });
@@ -37,8 +46,4 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-app.use('/api/vehiculos', require('./routes/vehiculos'));
-app.use('/api/usuarios', require('./routes/usuarios'));
-app.use('/api/celdas', require('./routes/celdas'));
-app.use('/api/pagos', require('./routes/pagos'));
-app.use('/api/novedades', require('./routes/novedades'));
+
